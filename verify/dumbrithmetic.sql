23988e07397c7f09d757dcec30f2d5d06ece6262
Salman Abdulqohar <salman.qohar@gmail.com>
2021-08-30 15:45:33 +0700

    remove hardcoded database

--- verify/dumbrithmetic.sql
+++ verify/dumbrithmetic.sql
@@ -3,6 +3,6 @@
 BEGIN;
 
 SELECT sqitch.checkit(COUNT(*), 'Table "dumbrithmetic is not exists"')
-  FROM mysql.innodb_index_stats WHERE database_name = 'arith' AND table_name = 'dumbrithmetic';
+  FROM mysql.innodb_index_stats WHERE table_name = 'dumbrithmetic';
 
 ROLLBACK;
